bf42979d9e50b6c847c1da30fdbebd17ac1a9e0c
Alec <alec.espiritu@gmail.com>
2018-12-20 10:41:48 -0800

    Update

---
+++ src/UserInput/UserInput.js
@@ -0,0 +1,13 @@
+import React from 'react';
+//import './UserInput.css';
+
+
+class UserInput extends Component {
+  render() {
+    return (
+      <div className="userInput">
+        <input type="text"/>
+      </div>
+    );
+  }
+}
---
+++ src/UserOutput/UserOutput.js
@@ -0,0 +1,14 @@
+import React from 'react';
+//import './UserInput.css';
+
+
+class UserOutput extends Component {
+  render() {
+    return (
+      <div className="userOutput">
+      	<p>Name: Alec</p>
+      	<p>Age: 28</p>
+      </div>
+    );
+  }
+}
